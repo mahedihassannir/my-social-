3562e80b1fe4805a4917282fa19f68b22a94e795
mahedi6666655555 <mahedinir34678@gmail.com>
2023-04-17 23:43:14 +0600

    website reivew is done

---
+++ src/compinents/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyUS1_BCM4gD3Y_y3GgoBbee5b_wv0_vY",
+  authDomain: "social-app-42333.firebaseapp.com",
+  projectId: "social-app-42333",
+  storageBucket: "social-app-42333.appspot.com",
+  messagingSenderId: "943921114301",
+  appId: "1:943921114301:web:3c04d6037bb998a6fa5bd4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/compinents/pages/Header.jsx
+++ src/compinents/pages/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png'
 import moment from 'moment';
-
+import { Button } from 'bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
@@ -10,8 +11,16 @@ const Header = () => {
 
             <div className='text-center'>
                 <img className='mt-5' src={logo} alt="" />
-                <p className=''><small className=''></small></p>
+                <p className='py-2'><small className=''>Journalism Without Fear or Favour</small></p>
                 <p className=''>{moment().format("dddd MMMM D YYYY")}</p>
+            </div>
+
+            <div className=' d-flex w-50 mx-auto'>
+                <button>latest </button>
+                <Marquee speed={100}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+
             </div>
 
 
